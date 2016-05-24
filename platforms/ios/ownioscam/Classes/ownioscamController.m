e82d11bf345f001b3db0fbf54e37a7dc59cffea4
wisemendevp <dev@vqs.com.au>
2016-05-24 16:27:59 +0530

    43.0

--- platforms/ios/ownioscam/Classes/ownioscamController.m
+++ platforms/ios/ownioscam/Classes/ownioscamController.m
@@ -15,18 +15,26 @@
         self.picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
         self.picker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
         self.picker.showsCameraControls = NO;
+    //    self.picker.wantsFullScreenLayout = yes;
+       // self.picker.cameraFlashMode = UIImagePickerControllerCameraFlashModeOn;
         
         // Make us the delegate for the UIImagePickerController
         self.picker.delegate = self;
      //   self.overlay = [[ownioscamController alloc] init @"ownioscam" bundle:nil]
                         
+        UIView * overlayView = [[UIView alloc] initWithFrame:self.picker.view.frame];
+        [overlayView.layer setOpaque:NO];
+        overlayView.opaque = NO;
+        
         // Set the frames to be full screen
-        CGRect screenFrame = [[UIScreen mainScreen] bounds];
-        self.view.frame = screenFrame;
-        self.picker.view.frame = screenFrame;
+      //  CGRect screenFrame = [[UIScreen mainScreen] bounds];
+       // self.view.frame = screenFrame;
+     //   self.picker.view.frame = screenFrame;
         
         // Set this VC's view as the overlay view for the UIImagePickerController
         self.picker.cameraOverlayView = self.view;
+       // [self presentedViewController : self.picker animated:true completion:nil];
+        [self presentModalViewController:self.picker animated:YES];
     }
     return self;
 }
@@ -35,34 +43,57 @@
 -(IBAction) takePhotoButtonPressed:(id)sender forEvent:(UIEvent*)event {
     // Call the takePicture method on the UIImagePickerController to capture the image.
     [self.picker takePicture];
+    
 }
 
 -(IBAction) cancel:(id)sender forEvent:(UIEvent*)event {
     // Call the takePicture method on the UIImagePickerController to capture the image.
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+   NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     
     
     NSString * imgarray = [defaults objectForKey:@"k1"];
+      NSString * imgarray1 = [defaults objectForKey:@"k2"];
+    
+    imgarray = [imgarray stringByAppendingString:@";;" ];
+    imgarray = [imgarray stringByAppendingString:imgarray1 ];
+    
     
     [self.plugin capturedImageWithPath:imgarray];
     [defaults removeObjectForKey:@"k1"];
+      [defaults removeObjectForKey:@"k2"];
    
+}
+-(NSString*)generateRandomString:(int)num {
+    NSMutableString* string = [NSMutableString stringWithCapacity:num];
+    for (int i = 0; i < num; i++) {
+        [string appendFormat:@"%C", (unichar)('a' + arc4random_uniform(25))];
+    }
+    return string;
+}
+
+-(UIInterfaceOrientation)preferredInterfaceOrientationForPresentation{
+    return UIInterfaceOrientationPortrait;
+}
+
+- (UIImage *)resizeImage:(UIImage*)image newSize:(CGSize)newSize {
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 // Delegate method.  UIImagePickerController will call this method as soon as the image captured above is ready to be processed.  This is also like an event callback in JavaScript.
 -(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
     // Get a reference to the captured image
-    UIImage* image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    UIImage* image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
     
     self.ImageView.image = image;
     
-    NSDate *currentDate = [NSDate date];
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"dd.MM.YY HH:mm:ss"];
-    NSString *dateString = [dateFormatter stringFromDate:currentDate];
+     NSString* string = [self generateRandomString:5];
+      NSString* compresed_string = [self generateRandomString:5];
     
-    NSString* string = dateString;
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     
     
@@ -70,18 +101,36 @@
     NSString* documentsDirectory = [paths objectAtIndex:0];
     NSString * s1 = @".jpg";
     string = [string stringByAppendingString:s1 ];
+     compresed_string = [compresed_string stringByAppendingString:s1 ];
     NSString* filename = string;
+     NSString* compressed_filename = compresed_string;
+    
+    
     NSString* imagePath = [documentsDirectory stringByAppendingPathComponent:filename];
+     NSString* compressed_imagePath = [documentsDirectory stringByAppendingPathComponent:compressed_filename];
     
     // Get the image data (blocking; around 1 second)
     NSData* imageData = UIImageJPEGRepresentation(image, 1.0);
     [imageData writeToFile:imagePath atomically:YES];
+    
+    
+    UIImage *newImage=image;
+    CGSize size=CGSizeMake(110,110);
+    newImage=[self resizeImage:newImage newSize:size];
+    
+    
+    NSData* compressed_imageData = UIImageJPEGRepresentation(newImage, 0.5);
+    [compressed_imageData writeToFile:compressed_imagePath atomically:YES];
+    
+    
     NSString * str5 = [defaults objectForKey:@"k1"];
     //UIImage *img = [UIImage imageWithContentsOfFile:imagePath];
     // UIImageWriteToSavedPhotosAlbum(img,nil,nil,nil);
     if([str5 length] == 0)
     {
         [defaults setObject:imagePath forKey:@"k1"];
+        
+        [defaults setObject:compressed_imagePath forKey:@"k2"];
         
         
     }
@@ -92,6 +141,13 @@
         
         _sbuffer = [_sbuffer stringByAppendingString:imagePath];
         [defaults setObject:_sbuffer forKey:@"k1"];
+        
+        
+        NSString * _sbufferc =   [defaults objectForKey:@"k2"];
+        _sbufferc = [_sbufferc stringByAppendingString:@","];
+        
+        _sbufferc = [_sbufferc stringByAppendingString:compressed_imagePath];
+        [defaults setObject:_sbufferc forKey:@"k2"];
         
         
         
